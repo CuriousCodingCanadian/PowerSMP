f4ebc65a6ec965c482c3e255f426e7230bc1ad75
EEsa khokhar <khokhareesa.home@gmai.com>
2024-05-15 21:05:57 -0400

    ss

--- src/main/java/dev/ees4/powersmp/PowerSMP.java
+++ src/main/java/dev/ees4/powersmp/PowerSMP.java
@@ -1,10 +1,14 @@
 package dev.ees4.powersmp;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PowerSMP implements DedicatedServerModInitializer {
+
+    public final Logger LOGGER = LoggerFactory.getLogger("PowerSMP");
     @Override
     public void onInitializeServer() {
-
+        LOGGER.info("PowerSMP Initialized!");
     }
 }
