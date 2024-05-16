84f6bf0e5e119d91218dc73bed2a56f0c84333a5
EEsa khokhar <khokhareesa.home@gmai.com>
2024-05-15 20:56:47 -0400

    ss

---
+++ src/main/java/dev/ees4/powersmp/PowerSMP.java
@@ -0,0 +1,10 @@
+package dev.ees4.powersmp;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class PowerSMP implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
